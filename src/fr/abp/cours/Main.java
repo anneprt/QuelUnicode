ea120c5173cf9b1656613d6a8da24654159a0698
Anne <patesdegeek@gmail.com>
2020-04-15 23:37:18 +0200

    lecture au clavier du caractere

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,13 @@
 package fr.abp.cours;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char recherche;
+        System.out.println("Quel caractère recherchez vous ?");
+        Scanner lectureClavier = new Scanner(System.in);
+        recherche = lectureClavier.next().charAt(0);
     }
 }
