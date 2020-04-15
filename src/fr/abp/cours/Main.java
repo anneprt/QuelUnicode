3ee2620d6c6b314488b3bff53fd270e57512ddaa
Anne <patesdegeek@gmail.com>
2020-04-15 23:46:07 +0200

    final commit

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -5,9 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int i;
+        char aTrouver;
         char recherche;
-        System.out.println("Quel caractère recherchez vous ?");
         Scanner lectureClavier = new Scanner(System.in);
+        System.out.println("Quel caractère recherchez vous ?");
         recherche = lectureClavier.next().charAt(0);
+
+        for (i = 0; i <= 127; i++) {
+            aTrouver = (char) i;
+            if (aTrouver == recherche) {
+                System.out.println("le code Unicode de " + aTrouver + " est \\u00" + Integer.toString(i, 16));
+            }
+        }
     }
 }
